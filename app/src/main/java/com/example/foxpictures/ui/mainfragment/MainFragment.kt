229cd4cab4aff997c4f7f1e14f650206b5127c4b
chrisstel <jashinisso_job@mail.ru>
2024-09-27 22:25:05 +0300

    Add animation, change button color

--- app/src/main/java/com/example/foxpictures/ui/mainfragment/MainFragment.kt
+++ app/src/main/java/com/example/foxpictures/ui/mainfragment/MainFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -11,6 +12,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.bumptech.glide.Glide
+import com.example.foxpictures.R
 import com.example.foxpictures.databinding.FragmentMainBinding
 import com.example.foxpictures.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,6 +22,7 @@ import kotlinx.coroutines.launch
 class MainFragment : Fragment() {
     private val viewModel: MainViewModel by viewModels()
     private var binding: FragmentMainBinding? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -37,7 +40,7 @@ class MainFragment : Fragment() {
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.currentImage.collect { imageResponseState ->
-                    when(imageResponseState) {
+                    when (imageResponseState) {
                         is Resource.Successful -> onSuccess(url = imageResponseState.data.url)
                         is Resource.Loading -> onLoading()
                         is Resource.Error -> onError(message = imageResponseState.message)
@@ -49,23 +52,51 @@ class MainFragment : Fragment() {
     }
 
     private fun onSuccess(url: String) = views {
-        foxImage.alpha = 1.0f
-        progressBar.visibility = View.INVISIBLE
-        getImageButton.isEnabled = true
+        showPicture(url)
+        removeLoadingState()
+        fadeIn()
+    }
+
+    private fun showPicture(url: String) = views {
         Glide.with(this@MainFragment).load(url).into(foxImage)
     }
+
+    private fun fadeIn() = views {
+        foxImage.alpha = 1f
+        foxImage.startAnimation(
+            AnimationUtils.loadAnimation(
+                context,
+                R.anim.fade_in
+            )
+        )
+    }
+
     private fun onLoading() = views {
+        setLoadingState()
+    }
+
+    private fun setLoadingState() = views {
         foxImage.alpha = 0.5f
         progressBar.visibility = View.VISIBLE
         getImageButton.isEnabled = false
     }
+
     private fun onError(message: String?) = views {
         foxImage.alpha = 0.5f
-        progressBar.visibility = View.INVISIBLE
-        getImageButton.isEnabled = true
-        Toast.makeText(context, message ?: "Something went wrong..Try again", Toast.LENGTH_LONG).show()
+        removeLoadingState()
+        showErrorMessage(message)
     }
+
+    private fun showErrorMessage(message: String?) {
+        Toast.makeText(context, message ?: "Something went wrong..Try again", Toast.LENGTH_LONG)
+            .show()
+    }
+
     private fun onDefault() = views {
+        removeLoadingState()
+    }
+
+    private fun removeLoadingState() = views {
         progressBar.visibility = View.INVISIBLE
         getImageButton.isEnabled = true
     }
